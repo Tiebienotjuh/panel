f0117dfe67ccb8945f98cd60f963b3ea17778a44
Tiebienotjuh <tiebe.seynhaeve@sgsintpaulus.eu>
2022-10-14 18:55:33 +0200

    Missing key added.

--- functions/language.js
+++ functions/language.js
@@ -2,6 +2,9 @@ const config = require('../config.json');
 
 function message(message) {
     let lang = require(`../locale/${config.application.language}.json`);
+    if(!lang[message]) {
+        return "Missing language key: " + message;
+    }
     return lang[message];
 }
 
