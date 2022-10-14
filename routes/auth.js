f3231d3f3e12a2df3e61d9d75bd2ec9611a2079f
Snikkertjuh <snikker.zakelijk@protonmail.com>
2022-10-14 10:16:54 +0200

    Added session to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require("express")
+const session = require("express-session")
 const { message } = require("../functions/language.js")
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/login", (req, res) => {
-    res.render("auth/login", {title: "Login"})
+    res.render("auth/login", {title: "Login", session: session})
 })
 
-module.exports = router;
+module.exports = router
