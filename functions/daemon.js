4abeca781f61b518d0229e195f37a24724292c3a
Tiebienotjuh <tiebe.seynhaeve@sgsintpaulus.eu>
2022-10-15 14:27:25 +0200

    added file editor (not styled)

--- functions/daemon.js
+++ functions/daemon.js
@@ -1,12 +1,14 @@
 const config = require('../config.json');
 
-function message(key) {
+function get(key) {
     let lang = require(`../locale/${config.application.language}.json`);
     if(!lang[key]) {
         return "Missing language key: " + key;
     }
     return lang[key];
 }
+
+
 
 module.exports = {
     message
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const session = require("express-session")
+const fs = require("fs");
 const { message } = require("../functions/language.js")
 
 const router = express.Router()
@@ -8,6 +9,12 @@ router.get("/login", (req, res) => {
     res.render("auth/login", {title: message("Login"), m: message})
 })
 
+router.get("/test", (req, res) => {
+    fs.readFile('./config.json', (e, data) => {
+        if (e) throw e;
+        res.render("file/edit", {title: message("Login"), m: message, data: data})
+    });
+})
 router.get("/register", (req, res) => {
     res.render("auth/register", {title: message("Register"), m: message})
 })
