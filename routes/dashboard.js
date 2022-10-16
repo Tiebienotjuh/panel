b6b6522d6cffa2e7711d97b3e33e0d659cdc35cc
Tiebienotjuh <tiebe.seynhaeve@sgsintpaulus.eu>
2022-10-16 12:03:59 +0200

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const mysql = require("mysql")
-const { message } = require("../functions/language.js")
-const { loggedin } = require("../functions/loggedin");
 const config = require("../config.json")
-const db = require("../functions/database.js")
+const db = require("../functions/database.js");
+const { message } = require("../functions/language.js")
+const { isAuth } = require("../functions/auth.js");
 
 const router = express.Router()
 
-router.get("/dashboard", (req, res) => {
-    loggedin(req, res, () => {
-        res.render("dashboard/dashboard", {title: "Dashboard", m: message, pannelname: config.application.name})
-    })
-})
+app.get("/", isAuth, (req, res) => {
+    res.redirect('/dashboard')
+});
+
+router.get("/dashboard", isAuth, (req, res) => {
+    res.render("dashboard/dashboard", {title: "Dashboard", m: message, pannelname: config.application.name})
+});
+
+router.get("/dashboard", isAuth, (req, res) => {
+    let sql = mysql.format("SELECT * FROM servers WHERE user_id = ?", [req.session.userid]);
+    db.query(sql, function (err, data) {
+        if (err) throw err;
+        res.render("dashboard/dashboard", {title: "Dashboard", m: message, panelname: config.application.name, servers: data})
+    });
+});
 
 module.exports = router
